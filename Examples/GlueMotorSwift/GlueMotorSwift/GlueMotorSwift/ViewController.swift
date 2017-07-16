b8ff1cc74658885106164ebddd56c189ae84e905
Kurt Bowen <kurtisbowen99@gmail.com>
2017-07-16 11:09:47 -0500

    Initial Commit Some Starter code. (I have no Idea what I'm Doing)

---
+++ Examples/GlueMotorSwift/GlueMotorSwift/GlueMotorSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlueMotorSwift
+//
+//  Created by Kurt Bowen on 7/16/17.
+//  Copyright © 2017 Kurt Bowen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+#IOS Sentry CAM
+This Project is a WIP App that will be able to control servos via a Iphone, target moving objects via tracking, and record while doing so. This project is far from completion.
